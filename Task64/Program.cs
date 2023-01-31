94496f189e54acb1f5590e4c0893c144217dd53f
Vikki <vikki_ka@mail.ru>
2023-01-31 18:16:22 +0300

    Init repo

---
+++ Task64/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetUserData(string massage)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(massage);
+    int UserData = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return UserData;
+}
+
+void GetRange(int N)
+{
+    if (N == 0) 
+    { 
+        Console.Write("\b" + "\b" + " ");
+        return;
+    }
+    Console.Write(N + ", ");
+    GetRange(N-1);
+}
+
+int N = GetUserData("Ввидите N:");
+GetRange(N);
---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30     */
+
+int GetUserData(string massage)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(massage);
+    int UserData = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return UserData;
+}
+
+int GetSumNumber(int start, int end)
+{
+    int sum = start;
+    if (start == end) return sum;
+    return sum + GetSumNumber(start + 1, end);
+}
+
+int M = GetUserData("Ввидите число М: ");
+int N = GetUserData("Ввидите число N: ");
+int sum = GetSumNumber(M, N);
+Console.WriteLine($"Сумма чисел от {M} до {N} равна = {sum}");
